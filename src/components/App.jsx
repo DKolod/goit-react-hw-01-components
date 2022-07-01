9266d8b54ad97cc9d093b1ad0381cb6c1288a5c5
Dmitry Kolodchenko <92672116+DKolod@users.noreply.github.com>
2022-07-01 23:45:04 +0300

    add profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import user from 'components/Profile/user.json';
+import Profile from 'components/Profile/Profile';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        stats={user.stats}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+      />
+      ggggg
     </div>
   );
 };
